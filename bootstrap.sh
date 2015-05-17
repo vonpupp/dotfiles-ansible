4f6eb4b47437d1be333d8af0ad7cab057e9c3c75
vonpupp <vonpupp@gmail.com>
2015-05-17 06:49:05 +0200

    Fixed stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,2 @@
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" $@
-ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" --ask-become-pass $@
+ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu,user_email=asf@mail.com" --ask-become-pass $@
