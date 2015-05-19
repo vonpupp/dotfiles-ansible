5c9cd6edf2f2a2d32c7b25f722953f4e568d8bed
vonpupp <vonpupp@gmail.com>
2015-05-19 23:06:40 +0000

    Added mount-nfs-cache task

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,5 @@
+
+#sudo pacman -Sy ansible
+
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" $@
 ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu, user_email=asf@mail.com" --ask-become-pass $@
