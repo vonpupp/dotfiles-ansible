b1552f7836b6135a587da51e02e1c7854eadd8d0
vonpupp <vonpupp@gmail.com>
2015-05-20 04:39:05 +0000

    Fixed minor issues, now everything seems to work

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@
 #sudo pacman -Sy ansible
 
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" $@
-ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu, user_email=asf@mail.com" --ask-become-pass $@
+#ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu, user_email=asf@mail.com" --ask-become-pass $@
+ansible-playbook setup.yml -i "local," --extra-vars "user_email=asf@mail.com" --ask-become-pass $@
