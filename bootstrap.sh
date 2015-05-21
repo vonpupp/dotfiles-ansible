abe2fb7678cdf292da60f5ec902718f18ead3775
vonpupp <vonpupp@gmail.com>
2015-05-21 01:53:24 +0000

    Added Stouts.users, Stouts.sudo and Stouts.locales roles from ansible-galaxy

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 #git submodule update
 #sudo pacman -Sy --noconfirm ansible
 
-#ansible-galaxy -p roles install Stouts.users
+#ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname
 
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" $@
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu, user_email=asf@mail.com" --ask-become-pass $@
