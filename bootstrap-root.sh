4a27ec7b616f99480c09e8125ca2facc7b0a3396
vonpupp <vonpupp@gmail.com>
2015-05-22 03:09:42 +0000

    Added xinitrc file

--- bootstrap-root.sh
+++ bootstrap-root.sh
@@ -1,1 +1,8 @@
-ansible-playbook setup-root.yml -i "local," --extra-vars "user=afu,makepkg_user=afu,user_email=asf@mail.com" --ask-become-pass $@
+
+#git submodule init
+#git submodule update
+#sudo pacman -Sy --noconfirm ansible
+
+#ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname
+
+ansible-playbook setup-root.yml -i "local," --extra-vars "user=afu, user_email=asf@mail.com" --ask-become-pass $@
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,4 @@
 
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" $@
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu, user_email=asf@mail.com" --ask-become-pass $@
-ansible-playbook setup.yml -i "local," --extra-vars "user_email=asf@mail.com" --ask-become-pass $@
+ansible-playbook setup.yml -i "local," --extra-vars "username=afu user_email=asf@mail.com" --ask-become-pass $@
