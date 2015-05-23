c0a1aa2b0e64a8be9848c0b8a713ba26010f7630
vonpupp <vonpupp@gmail.com>
2015-05-23 13:17:45 +0000

    Fixed user dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,4 @@
 
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" $@
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu, user_email=asf@mail.com" --ask-become-pass $@
-ansible-playbook setup.yml -i "local," --extra-vars "username=afu user_email=asf@mail.com" --ask-become-pass $@
+ansible-playbook setup.yml -i "local," --extra-vars "username=vagrant user_email=asf@mail.com" --ask-become-pass $@
