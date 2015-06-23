998064ff4c18cc34f64f93e42140d52f27e3109b
vonpupp <vonpupp@gmail.com>
2015-06-23 01:15:28 -0300

    Fixed bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,5 @@
 pacman -Sy --noconfirm ansible git
-git clone --recursive https://github.com/vonpupp/ansible-personal
-cd ansible-personal
-ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale
-Stouts.hostname
+ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname
 
-bootstrap-root.sh
-bootstrap-user.sh
+./bootstrap-root.sh
+#./bootstrap-user.sh
