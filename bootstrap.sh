8cc812a25283d70491b064141e0cb95a53e579ee
vonpupp <vonpupp@gmail.com>
2015-06-25 00:08:57 -0300

    Renamed root and user scripts and added some pacman and yaourt packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 pacman -Sy --noconfirm sudo ansible git
 ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname
 
-./bootstrap-root.sh
-#./bootstrap-user.sh
+./config-root.sh
+#./config-user.sh
