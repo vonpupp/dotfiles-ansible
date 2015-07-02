277dc42314930ea112a00900df6154efd74b9864
vonpupp <vonpupp@gmail.com>
2015-07-01 21:25:05 -0300

    Renamed files

---
+++ setup-user.sh
@@ -0,0 +1,10 @@
+
+#git submodule init
+#git submodule update
+#sudo pacman -Sy --noconfirm ansible
+
+#ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname
+
+#ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" $@
+#ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu, user_email=asf@mail.com" --ask-become-pass $@
+ansible-playbook playbook-user.yml -i "local," --extra-vars "username=av user_email=vonpupp@gmail.com" --ask-become-pass $@
