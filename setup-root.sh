e47101830ffbdee106631a66c59be1aebf011e89
vonpupp <vonpupp@gmail.com>
2015-10-10 19:27:00 -0300

    Cleaned setup-root.sh

--- setup-root.sh
+++ setup-root.sh
@@ -1,8 +1,2 @@
-
-#git submodule init
-#git submodule update
-#sudo pacman -Sy --noconfirm ansible
-
-#ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname
-
-ansible-playbook playbook-root.yml -i "local," --extra-vars "param_hostname=lex.flp.lan"
+#ansible-playbook playbook-root.yml -i "local," --extra-vars "param_hostname=sa4.flp.lan" --ask-become-pass $@
+ansible-playbook playbook-root.yml -i "local," --extra-vars "param_hostname=sa4.flp.lan" $@
