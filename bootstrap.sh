4b6ae61915b23142aa0c07a0d403fc01e947ad2a
root <root@archlinux.vagrant.vm>
2015-10-13 03:38:56 +1100

    Added: debug role

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@ git submodule init
 git submodule update
 
 pacman -Sy --noconfirm sudo ansible git
-ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname
+ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname f500.dumpall
 
-./config-root.sh
+#./config-root.sh
 #./config-user.sh
---
+++ setup-debug.sh
@@ -0,0 +1,1 @@
+ansible-playbook playbook-debug.yml -i "local," --extra-vars "my_user_name=av my_user_email=vonpupp@gmail.com" $@
