03e02c336195f44147b85dcbc6560fca34dcec22
vonpupp <vonpupp@gmail.com>
2016-10-13 23:43:23 -0300

    Add relative shell execution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+
+set -o nounset
+DIR="$(dirname $(readlink -f $0))"
+#source $DIR/vars.sh
+
 git submodule init
 git submodule update
 
 pacman -Sy --noconfirm sudo ansible git
 ansible-galaxy -p roles install Stouts.users Stouts.sudo Stouts.locale Stouts.hostname f500.dumpall
 
-#./config-root.sh
-#./config-user.sh
+$DIR/config-root.sh
+$DIR/config-user.sh
