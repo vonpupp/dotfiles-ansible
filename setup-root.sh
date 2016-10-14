684d580e5d233f4c8214614b3f54cd150375be43
vonpupp <vonpupp@gmail.com>
2016-10-13 23:49:52 -0300

    Add relative path execution

--- setup-root.sh
+++ setup-root.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+set -o nounset
+DIR="$(dirname $(readlink -f $0))"
+
 #ansible-playbook playbook-root.yml -i "local," --extra-vars "param_hostname=sa4.flp.lan" --ask-become-pass $@
-ansible-playbook playbook-root.yml -i "local," --extra-vars "param_hostname=sa4.flp.lan" $@
+ansible-playbook $DIR/playbook-root.yml -i "local," --extra-vars "param_hostname=sa4.flp.lan" $@
--- setup-user.sh
+++ setup-user.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -o nounset
+DIR="$(dirname $(readlink -f $0))"
 
 #git submodule init
 #git submodule update
@@ -7,7 +11,7 @@
 
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu" $@
 #ansible-playbook setup.yml -i "local," --extra-vars "makepkg_user=afu, user_email=asf@mail.com" --ask-become-pass $@
-ansible-playbook playbook-user.yml -i "local," --extra-vars "username=av user_email=vonpupp@gmail.com" --ask-become-pass $@
+ansible-playbook $DIR/playbook-user.yml -i "local," --extra-vars "username=av user_email=vonpupp@gmail.com" --ask-become-pass $@
 
 #ansible-playbook playbook-$(lsb_release -is | tr '[:upper:]' '[:lower:]')-user.yml -i "local," --extra-vars "username=av user_email=vonpupp@gmail.com" --ask-become-pass $@
 
